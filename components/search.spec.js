1da263967a97e409e1feae5347292f726f55ec99
Diego Almeida <diego.almeida@memed.com.br>
2022-12-18 10:33:49 -0300

    test(search): search unit tests

--- components/search.spec.js
+++ components/search.spec.js
@@ -1,22 +1,45 @@
 import Search from './search';
 import { render, screen, fireEvent } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
-const doSearch = jest.fn();
+const doSearch = jest.fn()
+
+const renderSearch = () => {
+  render(<Search doSearch={doSearch} />)
+}
 
 describe('Search', () => {
-  it('should render a form', () => {
-    render(<Search doSearch={doSearch} />);
 
-    expect(screen.getByRole('form')).toBeInTheDocument();
+  beforeEach(() => {
+    renderSearch()
+  })
+
+  it('Deve renderizar um form', () => {
+    expect(screen.getByRole('form')).toBeInTheDocument()
   });
 
-  it('should call props.doSearch() when form is submitted', async () => {
-    render(<Search doSearch={doSearch} />);
-
-    const form = screen.getByRole('form');
-
-    await fireEvent.submit(form);
-
-    expect(doSearch).toHaveBeenCalledTimes(1);
+  it('Deve renderizar um input do tipo "search"', () => {
+    expect(screen.getByRole('searchbox')).toHaveProperty('type', 'search')
   });
+
+  it('Deve chamar props.doSearch() quando for feito submit no formulário', async () => {
+    const form = screen.getByRole('form')
+    await fireEvent.submit(form)
+    expect(doSearch).toHaveBeenCalledTimes(1)
+  });
+
+  it('Deve chamar props.doSearch() com um input', async () => {
+    const form = screen.getByRole('form')
+
+    const inputText = 'any test'
+    const input = screen.getByRole('searchbox')
+
+    await userEvent.type(input, inputText)
+    await fireEvent.submit(form)
+
+    expect(doSearch).toHaveBeenCalledWith(inputText)
+    
+  });
+
+
 });
